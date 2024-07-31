38459b722bef7b8de09a20235fc3b143de735db8
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-07-31 19:08:12 +0530

    Update Start-Coding.java

--- Start-Coding.java
+++ Start-Coding.java
@@ -6,6 +6,11 @@
 copy the below code and execute in GFG
 
 
-
+  // Function to print hello
+    static void printHello(){
+        
+        System.out.println("Hello World");
+        
+    }
 
 */
