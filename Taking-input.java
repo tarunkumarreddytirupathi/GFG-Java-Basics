b0d2664d5ffc33510f7ddb19dd3678227c1d0bed
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-01 14:20:52 +0530

    Update Taking-input.java

--- Taking-input.java
+++ Taking-input.java
@@ -5,7 +5,27 @@
 
 copy the below code and execute in GFG
 
+class Geeks {
 
+    // Function to take input using Scanner class
+    static void IOFunction() {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+       
+
+        while (t-- > 0) {
+
+             int a = sc.nextInt();
+        float b=sc.nextFloat();
+        long c=sc.nextLong();
+        byte d=sc.nextByte();
+        sc.nextLine();
+        String s=sc.nextLine();
+        System.out.println(a+"\n"+b+"\n"+c+"\n"+d+"\n"+s);
+        }
+        
+    }
+}
 
 
 */
