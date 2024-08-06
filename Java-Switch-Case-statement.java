e8d0b9fa4027645f37278cf93bb0a2e4294bd6be
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-07 03:01:07 +0530

    Update Java-Switch-Case-statement.java

--- Java-Switch-Case-statement.java
+++ Java-Switch-Case-statement.java
@@ -9,8 +9,7 @@ copy the below code and execute in GFG
 
 class Solution{
     static double switchCase(int choice, List<Double> arr){
-        
-if(choice==1){
+        if(choice==1){
             return  3.14159*arr.get(0)*arr.get(0);
         }
         else{
