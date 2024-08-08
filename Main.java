2be4a3bf2965599a991694b51ed9433ddf49bc67
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-09 00:53:37 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,17 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
+
+    
+
     public static void main(String[] args) {
-        // Initialize an array of integers
-        int[] numbers = {1, 2, 3, 4, 5};
+        // Example input arrays with duplicates
+        int[] v1 = {4, 1, 3, 4, 2, 5, 1};
+        int[] v2 = {2, 4, 4, 1, 3, 3};
 
-        // Index of the element to be removed
-        int indexToRemove = 2; // Remove the element at index 2 (which is 3)
-
-        // Create a new array with a size one less than the original array
-        int[] newArray = new int[numbers.length - 1];
-
-        // Copy elements to the new array, excluding the element to remove
-        for (int i = 0, j = 0; i < numbers.length; i++) {
-            if (i != indexToRemove) {
-                newArray[j++] = numbers[i];
-            }
-        }
-
-        // Print the new array
-        System.out.println(newArray[1]);
+        // Get common elements without filtering duplicates
+        System.out.println( Integer.MIN_VALUE);
     }
 }
