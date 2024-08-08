2962b5483d2a1f9b1b0b72170969da5dd499e2f9
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-09 00:57:30 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,17 +1,39 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-
-public class Main {
-
+class Solution{
+    public static ArrayList<Integer> CommonElement(int v1[],int v2[],int n1,int n2,List<Integer> list){
+        if(v2==v2.length-1){
+            if(v1[n1]==v2[n2]){
+                list.add(v2[n2]);
+                v1[n1]=-1;
+                v2[v2]=-1;
+            }
+        }
+        else{
+            if(v1[n1]==v2[n2]){
+                list.add(v2[n2]);
+                v1[n1]=-1;
+                v2[v2]=-1;
+            }
+        }
+        return  CommonElement(v1,v2,v1, ++v2, list);
+        
+    }
     
-
-    public static void main(String[] args) {
-        // Example input arrays with duplicates
-        int[] v1 = {4, 1, 3, 4, 2, 5, 1};
-        int[] v2 = {2, 4, 4, 1, 3, 3};
-
-        // Get common elements without filtering duplicates
-        System.out.println( Integer.MIN_VALUE);
+    public static ArrayList<Integer> findCommonElements(int v1[],int v2[],int n1,int n2,List<Integer> list){
+        
+        if(v1==v1.length){
+            return findCommonElements(v1,v2,n1,0);
+        }
+        else{
+            return findCommonElements(v1,v2,++n1,0);
+        }
+    }
+    
+    public static ArrayList<Integer> common_element(int v1[], int v2[])
+    {
+        List<Integer> list=new ArrayList<>();
+        
+        list=findCommonElements(v1,v2,-1,-1,list);
+        
+        return list;
     }
 }
