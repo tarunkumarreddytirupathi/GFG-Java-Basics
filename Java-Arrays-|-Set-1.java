1b339cb174a2e6cf45152c35abe230377d2d90f7
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-11 04:21:19 +0530

    Update Java-Arrays-|-Set-1.java

--- Java-Arrays-|-Set-1.java
+++ Java-Arrays-|-Set-1.java
@@ -2,6 +2,19 @@
 
 copy the below code and execute in GFG
 
-
+class Solution {
+    public String average(int arr[]) {
+        
+        double sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        
+    
+        double average = sum / (double) arr.length;
+        
+        return String.format("%.2f", average);
+    }
+}
 
 */
