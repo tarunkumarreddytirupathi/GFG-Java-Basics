775248d78d1efbffefe8e99fc0492c649f421ed0
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-11 21:26:56 +0530

    Update Data-Types---Java.java

--- Data-Types---Java.java
+++ Data-Types---Java.java
@@ -4,6 +4,21 @@
 
 copy the below code and execute in GFG
 
-
+class Geeks{
+    
+    // Function to do operations with different data types
+    static void dataTypes(int a, float b, double c, long l, byte d){
+        
+        double p = c/b;
+        double q = b/a;
+        double r = c/a;
+        double m = r+l;
+        int s = a/d;
+        
+        //Printing all the results 
+        System.out.println(p + " " + q + " " + r + " " + m + " " + s);
+        
+    }
+}
 
 */
