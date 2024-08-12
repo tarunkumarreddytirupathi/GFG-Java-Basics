44d257f9d4f52b68354471ef7433e9b810335864
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-12 10:24:27 +0530

    Update Vowel-or-Not.java

--- Vowel-or-Not.java
+++ Vowel-or-Not.java
@@ -6,6 +6,16 @@
 copy the below code and execute in GFG
 
 
+//User function Template for Java
+class Solution{
+    String isVowel(char c){
+        c=Character.toLowerCase(c);
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+        return "YES";
+        else
+        return "NO";
+    }
+}
 
 
 */
