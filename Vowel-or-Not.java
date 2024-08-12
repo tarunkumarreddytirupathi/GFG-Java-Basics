02f619af1e406476e43031f51db2d5be75acb848
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-12 10:25:36 +0530

    Update Vowel-or-Not.java

--- Vowel-or-Not.java
+++ Vowel-or-Not.java
@@ -8,6 +8,7 @@ class Solution{
     String isVowel(char c){
         //convert character into lowercase letter 
         c=Character.toLowerCase(c);
+        
         if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
         return "YES";
         else
