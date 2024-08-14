b8108351a766c55ac18f5889420499df5d8aaabe
tarunkumarreddytirupathi <150784964+tarunkumarreddytirupathi@users.noreply.github.com>
2024-08-14 08:33:53 +0530

    Update Delete-alternate-characters.java

--- Delete-alternate-characters.java
+++ Delete-alternate-characters.java
@@ -6,6 +6,17 @@
 copy the below code and execute in GFG
 
 
-
+class Solution {
+    static String delAlternate(String S) {
+    String str="";
+    for(int i=0;i<S.length();i++){
+        if(i%2!=0)
+        continue;
+        char c=S.charAt(i);
+        str+=c;
+        }
+        return str;
+    }
+}
 
 */
