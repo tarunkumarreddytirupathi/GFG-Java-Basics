cbee743834981cffb9a267382d2e5441c6402679
tarun <tarunkumarreddytirupathi@gmail.com>
2024-08-18 05:40:48 +0530

    createa and update file

---
+++ Java-Input-Output.java
@@ -0,0 +1,15 @@
+/* 
+
+
+
+
+copy the below code and execute in GFG
+
+
+class Solution{
+    static void printIntString(String S,int N){
+        System.out.println("The input string :"+S+"\n"+"The input integer :"+N);
+    }
+}
+
+*/
