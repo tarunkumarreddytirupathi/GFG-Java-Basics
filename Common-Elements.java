6165729563e2eefdb695086520aa162d9e57591b
tarun kumar reddy tirupathi <tarunkumarreddytirupathi@gmail.com>
2024-09-23 19:57:10 +0530

    Add Solution class to find common elements in arrays

--- Common-Elements.java
+++ Common-Elements.java
@@ -6,6 +6,33 @@
 copy the below code and execute in GFG
 
 
+class Solution{
+    public static ArrayList<Integer> common_element(int v1[], int v2[])
+    {
         
+        Arrays.sort(v1);
+        Arrays.sort(v2);
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        int i = 0;
+        int j = 0;
+        
+        while(i < v1.length && j < v2.length) {
+            if(v1[i] == v2[j]) {
+                ans.add(v1[i]);
+                i++;
+                j++;
+            }
+            else if(v1[i] > v2[j]) {
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return ans;
+    }
+}
 
 */
