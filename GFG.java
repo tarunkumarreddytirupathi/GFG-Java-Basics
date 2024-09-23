cf47ebdad9c3c810946e05af86c9c8ef256e3099
tarun kumar reddy tirupathi <tarunkumarreddytirupathi@gmail.com>
2024-09-23 19:54:57 +0530

    Add Java file and solution for common elements

--- GFG.java
+++ GFG.java
@@ -1,29 +1,124 @@
-import java.util.Scanner;
+//{ Driver Code Starts
+//Initial Template for Java
 
-/**
- * GFG
- */
-public class GFG {
+/*package whatever //do not write package name here */
 
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=84;
-        // int a[][]=new int[n][n];
-        int b[]=new int[n];
-        for (int i = 0; i < n; i++) {
-            b[i]=sc.nextInt();
+// import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking total count of testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking total count-1 of elements 
+		    int n=sc.nextInt();
+		    
+		    //Declaring and Initializing an ArrayList-1
+		    int v1[]=new int[n];
+		    
+		    //adding elements to the ArrayList-1
+		    for(int i=0;i<n;i++)
+		    {
+		        v1[i]=sc.nextInt();
+		    }
+		    
+		    //taking total count-2 of elements 
+		    int m=sc.nextInt();
+		    
+		    //Declaring and Initializing an ArrayList-2
+		    int v2[]=new int[m];
+		    
+		    //adding elements to the ArrayList-2
+		    for(int i=0;i<m;i++)
+		    {
+		        v2[i]=sc.nextInt();
+		    }
+		    Solution ob = new Solution();
+		    //calling the method common_element
+		    //and passing ArrayList 1, 2 as arguments
+		    //and storing the results in a new ArrayList
+		    ArrayList<Integer>ans=ob.common_element(v1, v2);
+		    
+		    //printing the elements of the new ArrayList
+		    for(int i:ans)
+		    System.out.print(i+" ");
+		    
+		    System.out.println();
+
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+   
+    public static int secondForLoop(int v1[], int v2[],ArrayList<Integer> list, int first, int second){
+        if(second+1==v2.length){
+            if(v2[second]>0&&v1[first]==v2[second]){
+                list.add(v1[first]);
+                v2[second]=0;
+                return 1;
         }
+            return 1;
+        }
+        if(v2[second]>0&&v1[first]==v2[second]){
+                list.add(v1[first]);
+                v2[second]=0;
+                return 1;
+        }
+        ++second;
+        return secondForLoop(v1,v2,list,first,second);
+    }
         
-        int greater=0;
-        for(int i=0;i<n;i++){
-           
-               if(greater<b[i])
-               greater=b[i];
-               System.out.println(i+" "+greater);
-           
+   
+    public static int firstForLoop(int v1[], int v2[],ArrayList<Integer> list,int number){
+        if(number+1==v1.length){
+        //     for(int j=0;j<v2.length;j++){
+        //     if(v2[j]>0&&v1[number]==v2[j]){
+        //         list.add(v1[number]);
+        //         v2[j]=0;
+        //     }
+        // }
+        secondForLoop(v1,v2,list,number,0);
+            return  1;
         }
-        System.out.println(greater);
-        sc.close();
+        // for(int j=0;j<v2.length;j++){
+        //     if(v2[j]>0&&v1[number]==v2[j]){
+        //         list.add(v1[number]);
+        //         v2[j]=0;
+        //     }
+        // } 
+        secondForLoop(v1,v2,list,number,0);
+        ++number;
+        return firstForLoop(v1,v2,list,number);
+        
+       
+    }
     
+    public static ArrayList<Integer> common_element(int v1[], int v2[])
+    {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        //remove first for loop
+        firstForLoop(v1,v2,list,0);
+        // for(int i=0;i<v1.length;i++){
+        //     for(int j=0;j<v2.length;j++){
+        //     if(v1[i]>0&&v1[i]==v2[j]){
+        //         list.add(v1[i]);
+        //         v1[i]=0;
+        //         v2[j]=0;
+        //     }
+        // }    
+        // }
+        Collections.sort(list);
+        return list;
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
+import java.util.Arrays;
+
 public class Main {
-
-    // Method to check if three integers are equal
-    public static boolean areAllEqual(int a, int b, int c) {
-        return a == b && b == c;
-    }
-
     public static void main(String[] args) {
-        // Calling the method with three integers
-        boolean result = areAllEqual(10, 10, 10);
-
-        // Print the result
-        System.out.println("Are all three numbers equal? " + result);
+        int[] numbers = {5, 2, 8, 3, 1, 1};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers)); // Output: [1, 2, 3, 5, 8]
     }
 }
